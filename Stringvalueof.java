e42e21eff1db1347311dbb066f45c37a8e1d7ef0
Nani14354 <122523279+Nani14354@users.noreply.github.com>
2023-02-03 19:40:05 +0530

    Create Stringvalueof.java

---
+++ Stringvalueof.java
@@ -0,0 +1,12 @@
+public class Stringvalueof {
+    public static void main(String[] args) {          
+        // Boolean to String  
+        boolean bol = true;    
+        boolean bol2 = false;    
+        String s1 = String.valueOf(bol);    
+        String s2 = String.valueOf(bol2);  
+        System.out.println(s1);  
+        System.out.println(s2);  
+    }  
+}  
+

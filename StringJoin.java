28c31e67ff77c3d6545710324a0053594abbbd02
Nani14354 <122523279+Nani14354@users.noreply.github.com>
2023-02-03 19:21:34 +0530

    Create StringJoin.java

---
+++ StringJoin.java
@@ -0,0 +1,9 @@
+public class StringJoin {
+    public static void main(String[] args) {          
+        String date = String.join("/","25","06","2018");    
+        System.out.print(date);    
+        String time = String.join(":", "12","10","10");  
+        System.out.println(" "+time);  
+    }  
+}  
+

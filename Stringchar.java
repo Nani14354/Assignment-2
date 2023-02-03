7d7cdba1f1fe6787b442ba58e0c1324825918038
Nani14354 <122523279+Nani14354@users.noreply.github.com>
2023-02-03 18:13:50 +0530

    Update Stringchar.java

--- Stringchar.java
+++ Stringchar.java
@@ -1,4 +1,4 @@
-public class CharAtExample{  
+public class Stringchar{  
     public static void main(String args[]){  
     String name="javatpoint";  
     char ch=name.charAt(4);//returns the char value at the 4th index  
